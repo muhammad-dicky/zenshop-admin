79540f1bc7b982aba06c52ec023b36916ca5bb70
Dicky <muhammad.dickynn@gmail.com>
2023-11-29 18:10:23 +0700

    Update dependencies and add ClerkProvider to layout component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+    <div className="p-4">
+
+  
+    <p>Hello admin dashboard</p>
+    <Button >wasd</Button>
+    </div>
+    </>
+  )
+}
