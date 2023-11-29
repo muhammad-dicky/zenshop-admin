e0abf0c106e94bfde10d208f11a1f394d7706647
Dicky <muhammad.dickynn@gmail.com>
2023-11-30 00:05:23 +0700

    Add modal provider and dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,27 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect } from 'react';
 
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+
   return (
     <>
     <div className="p-4">
 
-  
-    <p>Hello admin dashboard</p>
-    <Button >wasd</Button>
+  Root Page
     </div>
     </>
   )
