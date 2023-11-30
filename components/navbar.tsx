5a44fef3c05b7896248f71b23a65bc55cc56236e
Dicky <muhammad.dickynn@gmail.com>
2023-11-30 19:50:29 +0700

    Add StoreSwitcher component to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
+import StoreSwitcher from "./store-switcher";
 
 const Navbar = () => {
     return ( 
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
                 <div>
-                    This will be a store switch
+                    <StoreSwitcher/>
                 </div>
                 <div>
                    <MainNav  className="px-6"/>
