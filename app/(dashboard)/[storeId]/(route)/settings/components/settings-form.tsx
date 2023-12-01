29eee7268d4ae928bd7af44213a82228a98b2188
Dicky <muhammad.dickynn@gmail.com>
2023-12-01 19:28:02 +0700

    Add form validation and input fields to SettingsForm component

--- app/(dashboard)/[storeId]/(route)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(route)/settings/components/settings-form.tsx
@@ -1,7 +1,16 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 
 
@@ -10,12 +19,31 @@ interface SettingsFormProps{
 }
 
 
+const formSchema = z.object({
+    name: z.string().min(1),
+});
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 
 
 const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    })
+
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data);
+    }
+
+
     return ( 
+        <>
         <div className="flex items-center justify-between">
             <Heading
             title="Settings"
@@ -23,7 +51,31 @@ const SettingsForm: React.FC<SettingsFormProps> = ({
             />
             <Button variant="destructive" size="icon" ><Trash className="h-4 w-4"/></Button>
         </div>
+        <Separator/>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-x-8 w-full">
+                <div className="grid grid-cols-3 gap-8">
+                    <FormField 
+                    control={form.control}
+                    name="name"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input disabled={loading} placeholder="Store name" {...field}/>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                    >
+
+                    </FormField>
+                </div>
+            </form>
+        </Form>
+        </>
      );
 }
- 
+
+
 export default SettingsForm;
+ 
