97b1a9bd4ba9ef2de558aa1d67f39bf126bae843
Dicky <muhammad.dickynn@gmail.com>
2023-12-26 04:03:25 +0700

    Add dashboard components and fetch data

---
+++ actions/get-stock-count.ts
@@ -0,0 +1,13 @@
+import prismadb from "@/lib/prismadb"
+
+
+
+export const getStockCount  = async (storeId: string) => {
+    const paidOrders = await prismadb.product.count({
+        where:{
+            storeId: storeId,
+        }
+    });
+
+    return paidOrders;
+}
