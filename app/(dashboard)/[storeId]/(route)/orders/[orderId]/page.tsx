84ab5e082bf3fa2fe04d01819c2a91c9982df74a
Dicky <muhammad.dickynn@gmail.com>
2023-12-26 23:11:52 +0700

    Remove unused import and update code in page.tsx

--- app/(dashboard)/[storeId]/(route)/orders/[orderId]/page.tsx
+++ app/(dashboard)/[storeId]/(route)/orders/[orderId]/page.tsx
@@ -1,5 +1,4 @@
 import prismadb from "@/lib/prismadb"
-import BillboardForm from "./components/order-form";
 
 
 
@@ -17,7 +16,7 @@ params: { billboardId: string }
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardForm initialData={billboard}/>
+                test
             </div>
         </div>
     )
