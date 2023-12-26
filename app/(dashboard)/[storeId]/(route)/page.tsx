354891377913412f02fe995c839ea83e655e56ef
Dicky <muhammad.dickynn@gmail.com>
2023-12-26 22:39:40 +0700

    Add recharts library and display graph revenue in dashboard page

--- app/(dashboard)/[storeId]/(route)/page.tsx
+++ app/(dashboard)/[storeId]/(route)/page.tsx
@@ -1,6 +1,8 @@
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
 import { getSalesCount } from "@/actions/get-sales-count";
 import { getStockCount } from "@/actions/get-stock-count";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
+import Overview from "@/components/overview";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
@@ -18,6 +20,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
     const totalRevenue = await getTotalRevenue(params.storeId);
     const salesCount = await getSalesCount(params.storeId);
     const stockCount = await getStockCount(params.storeId);
+    const graphRevenue = await getGraphRevenue(params.storeId);
 
 
     const store = await prismadb.store.findFirst({
@@ -74,6 +77,14 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                     </Card>
                     
                 </div>
+                <Card className="col-span-4">
+                    <CardHeader>
+                        <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2">
+                        <Overview data={graphRevenue}/>
+                    </CardContent>
+                </Card>
             </div>
         </div>
      );
