bab45bc1a074d4c3cb9e271f55699e95b49f1b49
Dicky <muhammad.dickynn@gmail.com>
2024-02-17 21:45:22 +0700

    Refactor layout and update middleware configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ModalProvider } from '@/providers/modal-providers'
-import { ToasterProvider } from '@/providers/toast-providers'
-import { ThemeProvider } from '@/providers/theme-provider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-providers";
+import { ToasterProvider } from "@/providers/toast-providers";
+import { ThemeProvider } from "@/providers/theme-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        <ToasterProvider/>
-        <ModalProvider/>
-        {children}
-
-        </ThemeProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+            {/* testing */}
+          </ThemeProvider>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
